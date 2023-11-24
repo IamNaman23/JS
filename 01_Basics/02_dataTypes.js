a39fc6121ccd5562514460ef0e09732ed1d0d954
IamNaman23 <86058235+IamNaman23@users.noreply.github.com>
2023-11-24 12:09:02 +0000

    discussed

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"    // treat all JS code as newer version
+
+// alert(3+3)  --> we are using NodeJS and not browser
+
+// code readability should be high
+
+let name="naman"
+let age=25
+let isLoggedIn=true
+
+// number --> 2^53
+// string --> "" or ''
+// boolean --> true or false
+// null --> standalone value
+// undefined --> value is not assigned
+// symbol --> used to uniquely identify components in react
+
+
+// object
+
+console.log(typeof undefined); // output: undefined
+console.log(typeof naull); // output: object
